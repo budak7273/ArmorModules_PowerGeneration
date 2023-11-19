0df54121fb9767aa9f4136cfe76dc7602df8a80a
Rob B <computerguy440+gh@gmail.com>
2023-11-19 13:59:03 -0500

    v2.2.1

--- RepoUpdate.sh
+++ RepoUpdate.sh
@@ -1,1 +1,1 @@
-bash ../PublicRepoCopyScript.sh /c/Git/SF_ModProject/Mods/ArmorModules_PowerGeneration . ../CommonKeepFiles.txt ../CommonSkipFiles.txt -v
+bash ../PublicRepoCopyScript.sh /c/Git/SF_ModProject/Mods/ArmorModules_PowerGeneration . ../CommonKeepFiles.txt ../CommonSkipFiles.txt
